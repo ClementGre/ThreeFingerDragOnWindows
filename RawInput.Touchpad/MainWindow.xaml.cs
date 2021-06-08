be6001443512c98fc1a660b5b3245a286e2edcc0
emoacht <emotom@pobox.com>
2021-06-09 03:25:03 +0900

    Initial commit

---
+++ RawInput.Touchpad/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace RawInput.Touchpad
+{
+	public partial class MainWindow : Window
+	{
+		public string Status
+		{
+			get { return (string)GetValue(StatusProperty); }
+			set { SetValue(StatusProperty, value); }
+		}
+		public static readonly DependencyProperty StatusProperty =
+			DependencyProperty.Register("Status", typeof(string), typeof(MainWindow), new PropertyMetadata(null));
+
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private HwndSource _targetSource;
+
+		protected override void OnSourceInitialized(EventArgs e)
+		{
+			base.OnSourceInitialized(e);
+
+			_targetSource = PresentationSource.FromVisual(this) as HwndSource;
+			_targetSource?.AddHook(WndProc);
+
+			TouchpadHelper.RegisterInput(_targetSource.Handle);
+		}
+
+		private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+		{
+			switch (msg)
+			{
+				case TouchpadHelper.WM_INPUT:
+					var contacts = TouchpadHelper.ParseInput(lParam);
+
+					Status = string.Join(Environment.NewLine, contacts.Select(x => x.ToString()));
+					break;
+			}
+			return IntPtr.Zero;
+		}
+	}
+}
---
+++ RawInput.Touchpad/TouchpadContact.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RawInput.Touchpad
+{
+	public struct TouchpadContact : IEquatable<TouchpadContact>
+	{
+		public int ContactId { get; }
+		public int X { get; }
+		public int Y { get; }
+
+		public TouchpadContact(int contactId, int x, int y) =>
+			(this.ContactId, this.X, this.Y) = (contactId, x, y);
+
+		public override bool Equals(object obj) => (obj is TouchpadContact other) && Equals(other);
+
+		public bool Equals(TouchpadContact other) =>
+			(this.ContactId == other.ContactId) && (this.X == other.X) && (this.Y == other.Y);
+
+		public static bool operator ==(TouchpadContact a, TouchpadContact b) => a.Equals(b);
+		public static bool operator !=(TouchpadContact a, TouchpadContact b) => !(a == b);
+
+		public override int GetHashCode() => (this.ContactId, this.X, this.Y).GetHashCode();
+
+		public override string ToString() => $"Contact ID:{ContactId} Point:{X},{Y}";
+	}
+
+	internal class TouchpadContactCreator
+	{
+		public int? ContactId { get; set; }
+		public int? X { get; set; }
+		public int? Y { get; set; }
+
+		public bool TryCreate(out TouchpadContact contact)
+		{
+			if (ContactId.HasValue && X.HasValue && Y.HasValue)
+			{
+				contact = new TouchpadContact(ContactId.Value, X.Value, Y.Value);
+				return true;
+			}
+			contact = default;
+			return false;
+		}
+
+		public void Clear()
+		{
+			ContactId = null;
+			X = null;
+			Y = null;
+		}
+	}
+}
