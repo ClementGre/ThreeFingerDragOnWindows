cbfe28b8a054796efbc5b7b8aa161a9234f91b13
Clément Grennerat <clement.grennerat@free.fr>
2023-09-15 17:23:27 +0200

    Setup elevated privileges and run on startup.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using Microsoft.UI.Xaml;
 using ThreeFingersDragOnWindows.settings;
 using ThreeFingersDragOnWindows.touchpad;
 using ThreeFingersDragOnWindows.utils;
+using Microsoft.Win32.TaskScheduler;
 
 namespace ThreeFingersDragOnWindows;
 
@@ -26,6 +28,8 @@ public partial class App : Application {
         } else{
             HandlerWindow = new HandlerWindow(this);
         }
+
+        RegisterStartupTask();
     }
 
 
@@ -53,5 +57,41 @@ public partial class App : Application {
 
     public bool DoTouchpadRegistered(){
         return HandlerWindow.TouchpadRegistered;
+    }
+
+    public void RegisterStartupTask()
+    {
+        Console.WriteLine("Registering Startup Task...");
+        using TaskService taskService = new TaskService();
+        TaskFolder folder = taskService.RootFolder.CreateFolder("ThreeFingersDragOnWindows", null, false);
+
+        taskService.RootFolder.DeleteTask("ThreeFingersDragOnWindows on startup", false);
+        if (folder.Tasks.Count != 0) return;
+
+        // Create a new task definition
+        TaskDefinition taskDefinition = taskService.NewTask();
+
+        // Set task properties (description, etc.)
+        taskDefinition.RegistrationInfo.Description = "Starting ThreeFingersDragOnWindows on system startup with elevated privileges.";
+        taskDefinition.RegistrationInfo.Author = "Clément Grennerat";
+
+        // Create a trigger (when to start the task)
+        LogonTrigger logonTrigger = new LogonTrigger();
+        taskDefinition.Triggers.Add(logonTrigger);
+
+        // Set an action (what the task does)
+        ExecAction execAction = new ExecAction("explorer.exe", "threefingersdragonwindows://", null);
+        taskDefinition.Actions.Add(execAction);
+
+        // Register the task
+        folder.RegisterTaskDefinition(
+            "ThreeFingersDragOnWindows on startup", // Task name
+            taskDefinition,
+            TaskCreation.CreateOrUpdate,
+            null, // User credentials (null for current user)
+            null, // User account password
+            TaskLogonType.InteractiveToken);
+
+        Console.WriteLine("Task created successfully.");
     }
 }
